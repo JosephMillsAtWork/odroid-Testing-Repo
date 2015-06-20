bb88972df18009ffee7a1f8ad50af05f227253e7
JosephMillsAtWork <joseph@infiniteautomation.com>
2015-06-20 12:44:43 -0600

    first commit

---
+++ web/README.md
@@ -0,0 +1,15 @@
+This is a websocket and bootstrap applciation. 
+
+This is under heavey development
+
+What it will become
+
+	1) place to monitor all builds and look at the log files ect
+	2) way to kick off meta builds for the repo
+	3) way to push to the repo all the packages that are maybe not in there
+	4) check status of the schrrots
+	
+Running 
+```bash
+./websocketd --address=localhost --port=8088  --staticdir=. ./sd.sh	
+```
---
+++ web/js/bak/makeMenu.js
@@ -0,0 +1,14 @@
+function makeMenu(){
+var menu = "<!-- left hand side basr -->
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li><a href="index.html">Machines</a></li>
+            <li><a href="logs.html">Log Files</a></li>
+            <li><a href="stage3Logfile.html">Build</a></li>
+            <li><a href="packages.html">Packages</a></li>
+            <li><a href="repoReady.html">Ready</a></li>
+
+          </ul</div> "
+
+
+}
